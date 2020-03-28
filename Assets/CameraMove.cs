f574d11696ba8ed4a916187a5ff2072b9e8e682a
sayukichi <62783837+sayukichi@users.noreply.github.com>
2020-03-28 17:20:15 +0900

    asset

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = player.transform.position.x;//playerのX座標
+        float posy = player.transform.position.y;//playerのy座標
+        float posz = player.transform.position.z;//playerのz座標
+
+        transform.position = new Vector3(posx, posy +10, posz-10);
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, 5);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0,-5);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(5, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-5, 0, 0);
+        }
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(0, 2, 0);
+        }
+
+    }
+    private void OnCollisionStay(Collision hit)
+    {
+        if(hit.gameObject.tag == "Enemy")
+        {
+            transform.position = new Vector3(0, 2, 0);
+        }
+    }
+}
